7f9920a56ab6d7181229c9921217f692cceb3ec4
heneli627627 <102131609+hen627@users.noreply.github.com>
2023-12-21 17:33:02 +0100

    fungerande post/behöver rensas
    types ska in i core/types, interface in i customdatatypes.ts i core/types

--- backend/functions/postMenu/index.ts
+++ backend/functions/postMenu/index.ts
@@ -1,12 +1,54 @@
 import { sendResponse } from '../../responses/index';
 import {db} from '../../services/index'
 
+// export const menu_db: string = "menu_table";
 
-async function postMenu(body:Array<Object>) {
-    
+
+export interface Menu {
+    menuId: string;
+    wontons: Wontons[];
 }
 
-exports.handler = async () => {
-    
-    return sendResponse(200, { success: true });
+export interface Wontons {
+    name: string;
+    desc: string;
+    ingredients: string[];
+    price: number;
+}
+
+
+async function postMenu(menu: Menu) {
+   try {
+    const params = {
+        TableName: "menu_table",
+        Item: menu
+    }
+    await db.put(params).promise();
+    console.log("ok?");
+    return sendResponse(200, { success: true } );
+   } catch (error) {
+    console.error('DynamoDB Error:', error);
+    return sendResponse(500, { success: false, error: 'Internal server error' });
+   }
+}
+
+exports.handler = async (event) => {
+    console.log("Hello");
+    console.log(event.body);
+    // const MenuItems = JSON.parse(event.body);
+    // console.log(MenuItems);
+    // return postMenu(event.body);
+
+    try {
+        const requestBody = JSON.parse(event.body);
+        const newMenu: Menu = {
+            menuId: requestBody.menuId,
+            wontons: requestBody.wontons,
+        };
+        console.log(newMenu);
+        return postMenu(newMenu);
+    } catch (error) {
+        console.error('Error parsing request or constructing Menu:', error);
+        return sendResponse(400, { success: false, error: 'Bad request' });
+    }
 }
