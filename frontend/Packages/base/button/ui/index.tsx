ef86544381d0b6f210323330746257b291482b94
gurkvatten <93186588+gurkvatten@users.noreply.github.com>
2023-12-21 10:35:58 +0100

    add variables

--- frontend/Packages/base/button/ui/index.tsx
+++ frontend/Packages/base/button/ui/index.tsx
@@ -1,6 +1,7 @@
 import './style.scss';
 import { ReactNode } from 'react';
 import React from 'react';
+import { StyleTypes } from '@zocom/types';
 
 
 
@@ -12,13 +13,14 @@ export enum ButtonType {
 
 type ButtonProps = {
     type?: ButtonType
+    style?: StyleTypes
     children?: ReactNode | ReactNode[];
     onClick: () => void
     
 }
 
 
-export const Button = ({ type = ButtonType.REGULAR, children, onClick }: ButtonProps) => {
+export const Button = ({ type = ButtonType.REGULAR, style= StyleTypes.DEFAULT, children, onClick }: ButtonProps) => {
     return (
     <button className={`button__${type}`} onClick={() => onClick()}>{children}</button>)
     
