2da40d19440bfbaae4f0d2c254acd9d89e9039d2
rupnaz <yonexrobban@gmail.com>
2024-01-02 11:36:43 +0100

    added middy

--- backend/functions/getOrder/index.ts
+++ backend/functions/getOrder/index.ts
@@ -1,28 +1,26 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import middy from '@middy/core';
+import httpErrorHandler from '@middy/http-error-handler';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
 import * as AWS from 'aws-sdk';
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 const tableName = process.env.ORDER_TABLE;
 
-export const handler: APIGatewayProxyHandler = async () => {
-  try {
-    const params: AWS.DynamoDB.DocumentClient.ScanInput = {
-      TableName: tableName,
-    };
+const getOrderHandler: APIGatewayProxyHandler = async () => {
+  const params: AWS.DynamoDB.DocumentClient.ScanInput = {
+    TableName: tableName,
+  };
 
-    const result = await dynamoDb.scan(params).promise();
+  const result = await dynamoDb.scan(params).promise();
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(result.Items),
-    };
-  } catch (error) {
-    console.error('Error fetching orders:', error);
-
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to fetch orders' }),
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result.Items),
+  };
 };
+
+export const handler = middy(getOrderHandler)
+  .use(httpJsonBodyParser()) // Middleware to parse JSON request body
+  .use(httpErrorHandler()); // Middleware to handle HTTP errors
