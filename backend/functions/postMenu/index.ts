8910a4fce3bdaa555d2fe2b49d532044a530d3b5
heneli627627 <102131609+hen627@users.noreply.github.com>
2024-01-03 06:33:11 +0100

    Added middy and Joi for validation, Functionality finished
    Postmenu functionality finished which makes an easy path for getMenu and postOrder to be created.
    What is left to do is to separate the functions and create packages to follow the monorepo structure.

--- backend/functions/postMenu/index.ts
+++ backend/functions/postMenu/index.ts
@@ -1,36 +1,47 @@
 import { sendResponse } from '../../responses/index';
-import {db} from '../../services/index'
+import {db} from '../../services/index';
+import middy from '../../node_modules/@middy/core';
+import Joi from 'joi';
+import { APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2 } from 'aws-lambda';
 
-export interface Menu {
-    menuId: string;
-    wontons: Wontons[];
+//varje recept som egen item i DB
+//typa up lambdaeventet, APIGatewayProxyEventV2 representerar httpapi 
+//då vi använder http api så behövs ej json bodyparse.
+//yml IamRoleStatements för smidigare role management något att kolla på senare
+//env tablenames
+//primary key sort key
+
+//tomorrow rewrite schema using joi, use the code in discord. Fixa simpel get function
+//behöver dela up i packages
+
+export function validateSchema(schema) {
+  return {
+    before: async (request) => {
+      try {
+        await schema.validateAsync(JSON.parse(request.event.body));
+      } catch (error) {
+        return sendResponse(400, {error: error});
+      }
+    },
+  };
 }
 
-export interface Wontons {
-    name: string;
-    desc: string;
-    ingredients: string[];
-    price: number;
-}
-
-
-async function postMenu(menu: Menu) {
+async function postMenu(menu) {
    try {
     const params = {
         TableName: "menu_table",
         Item: menu
     }
     await db.put(params).promise();
-    console.log("ok?");
     return sendResponse(200, { success: true } );
    } catch (error) {
-    console.error('DynamoDB Error:', error);
     return sendResponse(500, { success: false, error: 'Internal server error' });
    }
 }
 
-exports.handler = async (event) => {
+const handlerFunction = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyStructuredResultV2> => {
     try {
+        console.log(event)
         const requestBody = JSON.parse(event.body);
         return postMenu(requestBody);
     } catch (error) {
@@ -38,3 +49,16 @@ exports.handler = async (event) => {
         return sendResponse(400, { success: false, error: 'Bad request' });
     }
 }
+
+export const menuSchema = Joi.object({
+  menuId: Joi.string().min(3).max(30).required(),
+  category: Joi.string().min(3).max(20).required(),
+  desc: Joi.string().min(3).max(200).optional(),
+  ingredients: Joi.array().items(Joi.string()).optional(),
+  price: Joi.number().min(3).max(1000).required(),
+});
+
+
+exports.handler = middy()
+.use (validateSchema(menuSchema))
+.handler(handlerFunction)
