90dc0a2e452e8066321ede95f7844d6ff0b1f9c7
Shirak22 <116073530+Shirak22@users.noreply.github.com>
2024-01-04 15:55:40 +0100

    Adding the header to the landing page

--- frontend/Packages/pages/landing-page/ui/index.tsx
+++ frontend/Packages/pages/landing-page/ui/index.tsx
@@ -1,5 +1,6 @@
 import "./index.scss"; 
 import {MenuItem} from "@zocom/menuitem";
+import {Header} from "@zocom/header";
 
 
 export function LandingsPage() {
@@ -14,16 +15,20 @@ export function LandingsPage() {
         ],
         price:9,
    }
-   
-    return (   
+
+    return (
+      <section className="landingPage">
+         <Header title="Hello " />
         <main className="menu_container">
-            <h1 className="menu_title">MENY</h1>
-            <section className="menu_products">
-               <MenuItem  {...item }/>
-               <MenuItem  {...item }/>
-               <MenuItem  {...item }/>
-               <MenuItem  {...item }/>
-            </section>
+          <h1 className="menu_title">MENY</h1>
+          <section className="menu_products">
+            <MenuItem {...item} />
+            <MenuItem {...item} />
+            <MenuItem {...item} />
+            <MenuItem {...item} />
+            <MenuItem {...item} />
+          </section>
         </main>
-     );
+      </section>
+    );
 }
