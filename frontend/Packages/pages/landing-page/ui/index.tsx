2c5b99f45a420fe870fc396582dab2dd92c052ce
Shirak22 <116073530+Shirak22@users.noreply.github.com>
2024-01-04 15:23:20 +0100

    #3 nuild and style Menu item

--- frontend/Packages/pages/landing-page/ui/index.tsx
+++ frontend/Packages/pages/landing-page/ui/index.tsx
@@ -1,12 +1,28 @@
 import "./index.scss"; 
+import {MenuItem} from "@zocom/menuitem";
+
 
 export function LandingsPage() {
+   let item = {
+        name:"Karlstad",
+        desc:"En god friterad wonton med smaker från de värmländska skogarna.",
+        ingredients:[
+           "kantarell",
+           "scharlottenlök",
+           "morot",
+           "bladpersilja"
+        ],
+        price:9,
+   }
    
-    return ( 
+    return (   
         <main className="menu_container">
             <h1 className="menu_title">MENY</h1>
             <section className="menu_products">
-                
+               <MenuItem  {...item }/>
+               <MenuItem  {...item }/>
+               <MenuItem  {...item }/>
+               <MenuItem  {...item }/>
             </section>
         </main>
      );
