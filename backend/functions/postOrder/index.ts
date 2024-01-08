0233d1f25eabd96c4b50e62ddb8fdaa054df04da
heneli627627 <102131609+hen627@users.noreply.github.com>
2024-01-08 09:31:19 +0100

    Update index.ts

--- backend/functions/postOrder/index.ts
+++ backend/functions/postOrder/index.ts
@@ -3,3 +3,71 @@
 //then each PK User#username will store the orders 
 //like this in the SK, ORDER#XXXXXX then orderdata has 
 //username orderid etc
+
+import { sendResponse } from '../../responses/index';
+import {db} from '../../services/index';
+import middy from '../../node_modules/@middy/core';
+import Joi from 'joi';
+import { APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2 } from 'aws-lambda';
+
+type OrderType = {
+  PK: string,
+  SK: string,
+  orderName: string,
+  customerName: string,
+  desc: string,
+  price: number,
+  orderId: string,
+  status: string
+}
+
+export function validateSchema(schema) {
+  return {
+    before: async (request) => {
+      try {
+        await schema.validateAsync(JSON.parse(request.event.body));
+      } catch (error) {
+        return sendResponse(400, {error: error});
+      }
+    },
+  };
+}
+
+async function postMenu(order: OrderType) {
+   try {
+    const params = {
+        TableName: "YumYumDB",
+        Item: order
+    }
+    await db.put(params).promise();
+    return sendResponse(200, { success: true } );
+   } catch (error) {
+    return sendResponse(500, { success: false, error: 'Internal server error' });
+   }
+}
+
+const handlerFunction = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyStructuredResultV2> => {
+    try {
+        console.log(event)
+        const requestBody = JSON.parse(event.body);
+        return postMenu(requestBody);
+    } catch (error) {
+        console.error('Error json parse', error);
+        return sendResponse(400, { success: false, error: 'Bad request' });
+    }
+}
+
+export const OrderSchema = Joi.object({
+  PK: Joi.string().min(3).max(30).required(),
+  SK: Joi.string().min(3).max(50).required(),
+  orderName: Joi.string().min(3).max(20).required(),
+  customerName: Joi.string().min(3).max(200).required(),
+  price: Joi.number().min(3).max(1000).required(),
+  orderId: Joi.string().min(3).max(200).required(),
+  status: Joi.string().min(3).max(200).required()
+});
+
+
+exports.handler = middy()
+.use (validateSchema(OrderSchema))
+.handler(handlerFunction)
