829cce812bad88fcb3186aa184a36246bde69b57
gurkvatten <93186588+gurkvatten@users.noreply.github.com>
2024-01-10 10:29:36 +0100

    added btn with link to eta

--- frontend/Packages/base/cart-item/ui/index.tsx
+++ frontend/Packages/base/cart-item/ui/index.tsx
@@ -4,15 +4,17 @@ import React from 'react';
 
 type cartItemProps = {
     title: string
+    quantity: number
     price: number
 
 }
 
-export const CartItem = ({ title, price }: cartItemProps) => {
+export const CartItem = ({ title, quantity, price }: cartItemProps) => {
 
     return (
         <div className="cart-item">
             <p className="cart-item__title">{title}</p>
+            <p className="cart-item__quantity">{quantity}</p>
             <span className="cart-item__space"></span>
             <p className="cart-item__price">{price}</p>
         </div>
