79f51c5fbae8564456e9c4fc02f2f08ed400dc08
simeedin <113066728+simeedin@users.noreply.github.com>
2024-01-11 16:16:06 +0100

    kvitto view

--- frontend/Packages/pages/receipt-page/ui/index.tsx
+++ frontend/Packages/pages/receipt-page/ui/index.tsx
@@ -1,8 +1,22 @@
 import './index.scss';
 import { Header } from '@zocom/header';
-import { Button, ButtonColor, ButtonSize } from '@zocom/button'
-import React from 'react';
+import { Button, ButtonSize } from '@zocom/button';
+import { useNavigate } from 'react-router-dom';
+import logo from '../../../shared/logo.png';
+import logoRed from '../../../shared/logo-red.png';
+import { PriceBox } from '@zocom/price-box';
 
 export const ReceiptPage = () => {
-    
+    const navigate =  useNavigate();
+    const price:number = 120;
+    return (
+        <div className='receipt-page'>
+            <Header logo={logo} />
+            <main className='receipt-container'>
+                <img className='logo-red' src={logoRed} alt="logoRed" />
+                <PriceBox price={price}/>
+            </main>
+            <Button size={ButtonSize.STRETCH} onClick={() => navigate('/') }>GÖR EN NY BESTÄLLNING</Button>
+        </div>
+    )
 }
