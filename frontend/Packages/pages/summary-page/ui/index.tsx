fe4fcd115b1bf1326477146b7c70efb12b90c8a3
gurkvatten <93186588+gurkvatten@users.noreply.github.com>
2024-01-11 13:32:40 +0100

    Update index.tsx

--- frontend/Packages/pages/summary-page/ui/index.tsx
+++ frontend/Packages/pages/summary-page/ui/index.tsx
@@ -6,11 +6,12 @@ import { Button, ButtonColor } from '@zocom/button';
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from "react-redux";
 import "./styles.scss"
+import { cartItemType } from "@zocom/types";
 
 export function SummaryPage () {
  const navigate = useNavigate();
  const cartItems = useSelector((state: any) => state.cart);
- const totalPrice = cartItems.reduce((total: number, item: any) => total + item.price * item.quantity, 0);
+ const totalPrice = cartItems.reduce((total: number, item: cartItemType) => total + item.price * item.quantity, 0);
  
 
    
@@ -19,8 +20,8 @@ export function SummaryPage () {
     <div className="App">
       <Header title="Header" />
       <ul className="cart-items">
-        {cartItems.map((item, index) => (
-          <CartItem key={index} title={item.title} quantity={item.quantity} price={item.price} />
+        {cartItems.map((item: cartItemType, index:number) => (
+          <CartItem key={index} title={item.name} quantity={item.quantity} price={item.price} />
         ))}
       </ul>
       <span className="cart-space"></span>
